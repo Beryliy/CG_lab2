b9ca2aeaddb5eeed5944d4bead657b732584978b
Beryliy <urenad53@ukr.net>
2019-09-23 15:08:21 +0300

    initial commit

---
+++ desktop/src/com/flogiston/cg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.flogiston.cg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.flogiston.cg.Lab2;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Lab2(), config);
+	}
+}
